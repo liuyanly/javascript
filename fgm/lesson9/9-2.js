5c52fee211424f437db420e28bba311b59f4470f
liuyanly <464861732@qq.com>
2017-06-14 17:30:16 +0800

    lesson9 (2-3)

--- fgm/lesson9/9-2.js
+++ fgm/lesson9/9-2.js
@@ -79,7 +79,7 @@ window.onload = function() {
     ran && aData.sort(function(a,b) {return Math.random() > 0.5 ? -1 : 1});
 
     //插入结构
-    var oFragment = document.createDocumentFragMent();
+    var oFragment = document.createDocumentFragment();
     for (var i = 0; i < aData.length; i++) {
       var oLi = document.createElement("li");
       var oImg = document.createElement("img");
@@ -94,8 +94,172 @@ window.onload = function() {
     for (var i = 0; i < aLi.length; i++) {
       aLi[i].index = i;
       aLi[i].style.top = aLi[i].offsetTop + "px";
-      ali[i].style.left = aLi[i].offsetLeft + "px";
+      aLi[i].style.left = aLi[i].offsetLeft + "px";
       aPos.push({"left": aLi[i].offsetLeft, "top": aLi[i].offsetTop})
     };
+    for (var i = 0; i < aLi.length; i++) {
+      aLi[i].style.position = "absolute";
+      aLi[i].style.margin = "0";
+      drag(aLi[i])
+    };
+    //拖拽函数
+    function drag(obj, handle){
+      var handle = handle || obj;
+      handle.style.cursor = "move";
+      handle.onmousedown = function(event){
+        var event = event || window.event;
+        var disX = event.clientX - this.offsetLeft;
+        var disY = event.clientY - this.offsetTop;
+        var oNear = null;
+        obj.style.zIndex = zIndex++;
+        document.onmousemove = function() {
+          var event = event || window.event;
+          var iL = event.clientX - disX;
+          var iT = event.clientY - disY;
+          var maxL = obj.parentNode.clientWidth - obj.offsetWidth;
+          var maxT = obj.parentNode.clientHeight - obj.offsetHeight;
+
+          iL < 0 && (iL = 0);
+          iT < 0 && (iT = 0);
+          iL > maxL && (iL = maxL);
+          iT > maxT && (iT = maxT);
+          obj.style.left = iL + "px";
+          obj.style.top = iT + "px";
+
+          for (var i = 0; i < aLi.length; i++) {
+            aLi[i].className = "";
+          };
+
+          oNear = findNearest(obj);
+
+          oNear && (oNear.className = "hig");
+
+          return false
+        }
+        document.onmouseup = function() {
+          document.onmousemove = null;
+          document.onmouseup = null;
+          if(oNear) {
+            var tIndex = obj.index;
+            obj.index = oNear.index;
+            oNear.index = tIndex;
+            startMove(obj, aPos[obj.index]);
+            startMove(oNear, aPos[oNear.index], function() {
+              if(finish()){
+                var iHour = iMin = iSec = 0;
+                var oDateNow = new Date();
+                var iRemain = parseInt((oDateNow.getTime() - oDateStart.getTime())/1000);
+
+                iHour = parseInt(iRemain / 3600);
+                iRemain %= 3600;
+                iMin = parseInt(iRemain / 60);
+                iRemain %= 60;
+                iSec = iRemain;
+
+                alert("恭喜您，拼图完成！\n\n用时："+iHour+"小时"+iMin+"分"+iSec+"秒");
+                createMask();
+              }
+            });
+            oNear.className = "";
+          }else{
+            startMove(obj, aPos[obj.index])
+          }
+          handle.releaseCapture && handle.releaseCapture()
+        }
+        this.setCapture && this.setCapture();
+        return false
+      }
+    }
+    //找出相遇点中的最后元素
+    function findNearest(obj) {
+      var filterLi = [];
+      var aDistance = [];
+      for (var i = 0; i < aLi.length; i++) {
+        aLi[i] != obj && (isButt(obj,aLi[i]) && (aDistance.push(getDistance(obj, aLi[i])), filterLi.push(aLi[i])));
+      };
+
+      var minNum = Number.MAX_VALUE;
+      var minLi = null;
+
+      for (var i = 0; i < aDistance.length; i++) {
+        aDistance[i] < minNum && (minNum = aDistance[i], minLi = filterLi[i]);
+      };
+
+      return minLi
+    }
+  }
+  GAME();
+
+  //开始游戏
+  oInput.onclick = function() {
+    oDateStart = new Date();
+    oBox.innerHTML = "";
+    this.value = "重新开始";
+    GAME(true)
+  }
+  //判断是否完成
+  function finish(){
+    var aTemp = [];
+    var success = true;
+    aTemp.length = 0;
+    for (var i = 0; i < aLi.length; i++) {
+      for (var j = 0; j < aLi.length; j++) {
+        i == aLi[j]["index"] && aTemp.push(aLi[j].getElementsByTagName("img")[0].src.match(/(\d+)\./)[1])
+      };
+
+    };
+    for (var i = 1; i < aTemp.length; i++) {
+      if( i != aTemp[i-1]){
+        success = false;
+        break
+      }
+    };
+    return success
+  }
+}
+//求两点之间的距离
+function getDistance(obj1, obj2) {
+  var a = (obj1.offsetLeft + obj1.offsetWidth / 2) - (obj2.offsetLeft + obj2.offsetWidth / 2);
+  var b = (obj1.offsetTop + obj1.offsetHeight /2) - (obj2.offsetTop + obj2.offsetHeight / 2);
+  return Math.sqrt(a * a + b * b);
+}
+//踫撞检测
+function isButt(obj1, obj2) {
+  var l1 = obj1.offsetLeft;
+  var t1 = obj1.offsetTop;
+  var r1 = obj1.offsetLeft + obj1.offsetWidth;
+  var b1 = obj1.offsetTop + obj1.offsetHeight;
+
+  var l2 = obj2.offsetLeft;
+  var t2 = obj2.offsetTop;
+  var r2 = obj2.offsetLeft + obj2.offsetWidth;
+  var b2 = obj2.offsetTop + obj2.offsetHeight;
+
+  return !(r1 < l2 || b1 < t2 || r2 < l1 || b2 < t1)
+}
+//获取最终样式
+function getStyle(obj, attr) {
+  return parseFloat(obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj, null)[attr])
+}
+//运动框架
+function startMove(obj,pos,onEnd){
+  clearInterval(obj.timer);
+  obj.timer = setInterval(function() {
+    doMove(obj, pos, onEnd)
+  },30)
+}
+function doMove(obj, pos, onEnd) {
+  var iCurL = getStyle(obj, "left");
+  var iCurT = getStyle(obj, "top");
+  var iSpeedL = (pos.left - iCurL) / 5;
+  var iSpeedT = (pos.top - iCurT) / 5;
+  iSpeedL = iSpeedL > 0 ? Math.ceil(iSpeedL) : Math.floor(iSpeedL);
+  iSpeedT = iSpeedT > 0 ? Math.ceil(iSpeedT) : Math.floor(iSpeedT);
+  if(pos.left == iCurL && pos.top == iCurT){
+    clearInterval(obj.timer)
+    onEnd && onEnd()
+  }else{
+    obj.style.left = iCurL + iSpeedL + "px";
+    obj.style.top = iCurT + iSpeedT + "px";
   }
 }
