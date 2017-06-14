d5b2e5426bed3a95fa2fe81636e15cf9744bcf61
liuyanly <464861732@qq.com>
2017-06-14 22:13:11 +0800

    lesson10(1-2)

---
+++ fgm/lesson10/10-1.js
@@ -0,0 +1,18 @@
+//求和
+Array.prototype.sum = function() {
+	for(var sum = i = 0; i < this.length; i++) {
+		sum += this[i]
+	}
+	return sum
+}
+//求最大值
+Array.prototype.maxima = function() {
+	for(var i = 0, maxValue = Number.MIN_VALUE; i < this.length; i++) {
+		parseInt(this[i]) > maxValue && (maxValue = this[i]);
+	}
+	return maxValue
+}
+//应用
+var arr = [1,21,3,4,22,45,6,7,32];
+alert(arr.join("+")+"="+arr.sum());
+alert(arr.join("|") + "中，最大的数是:" + arr.maxima());
