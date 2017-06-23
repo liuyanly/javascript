3b8d8bec70c361ce460626097dcc821a5ff61e9c
liuyanly <464861732@qq.com>
2017-06-23 17:32:49 +0800

    lesson 10(8)

--- fgm/lesson10/10-7.js
+++ fgm/lesson10/10-7.js
@@ -1,11 +1,10 @@
 var fgm = {
   on: function(element, type, handler) {
-    return element.addEventListener ? element.addEventListener(type,handler,false):
-    element.attachEvent("on"+type, handler);
+    return element.addEventListener ? element.addEventListener(type, handler, false) : element.attachEvent("on" + type, handler)
   },
   bind: function(object, handler) {
-    return function(){
-      return handler.apply(object,arguments)
+    return function() {
+      return handler.apply(object, arguments)
     }
   },
   pageX: function(element) {
@@ -17,19 +16,21 @@ var fgm = {
   hasClass: function(element, className) {
     return new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className)
   },
-  attr: function(element,attr,value) {
+  attr: function(element, attr, value) {
     if(arguments.length == 2) {
-      return element.attributes[attr]?element.attributes[attr].nodeValue : undefined
-    }else if(arguments.length == 3) {
+      return element.attributes[attr] ? element.attributes[attr].nodeValue : undefined
+    }
+    else if(arguments.length == 3) {
       element.setAttribute(attr, value)
     }
   }
-}
+};
 //延时加载
 function LazyLoad(obj) {
   this.lazy = typeof obj === "string" ? document.getElementById(obj) : obj;
   this.aImg = this.lazy.getElementsByTagName("img");
   this.fnLoad = fgm.bind(this, this.load);
+  this.load();
   fgm.on(window, "scroll", this.fnLoad);
   fgm.on(window, "resize", this.fnLoad);
 }
@@ -44,28 +45,24 @@ LazyLoad.prototype = {
     var iBottom = 0;
     var aNotLoaded = this.loaded(0);
     if(this.loaded(1).length != this.aImg.length) {
-      for (i = 0; i < aNotLoaded.length; i++) {
+      for(i = 0; i < aNotLoaded.length; i++) {
         oParent = aNotLoaded[i].parentElement || aNotLoaded[i].parentNode;
         iTop = fgm.pageY(oParent);
         iBottom = iTop + oParent.offsetHeight;
-        if((iTop > iScrollTop && iTop < iClientHeight) ||
-          (iBottom > iScrollTop && iBottom < iClientHeight)) {
-          aNotLoaded[i].src = fgm.attr(aNotLoaded[i], "data-img" ||
-          aNotLoaded[i].src);
-          aNotLoaded[i].className = "loaded"
+        if((iTop > iScrollTop && iTop < iClientHeight) || (iBottom > iScrollTop && iBottom < iClientHeight)) {
+          aNotLoaded[i].src = fgm.attr(aNotLoaded[i], "data-img") || aNotLoaded[i].src;
+          aNotLoaded[i].className = "loaded";
         }
-      };
+      }
     }
   },
   loaded: function(status) {
     var array = [];
     var i = 0;
-    for (i = 0; i < this.aImg.length; i++) {
-      eval("fgm.className(this.aImg[i], \"loaded\")") + (!!status ? "&&" : "||") +
-      "array.push(this.aImg[i])";
-      return array
-    };
+    for(i = 0; i < this.aImg.length; i++)
+    eval("fgm.hasClass(this.aImg[i], \"loaded\")" + (!!status ? "&&" : "||") + "array.push(this.aImg[i])");
+    return array
   }
 };
 //应用
-fgm.on(window, "load", function() {new LazyLoad("box")});
+fgm.on(window, "load", function () {new LazyLoad("box")});
