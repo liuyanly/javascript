1e61ca7fc706ebd64c9eafc2fff880f61529bbb6
liuyanly <464861732@qq.com>
2017-07-19 17:38:00 +0800

    ls6(1)

---
+++ fgm2/5/5-8.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  var oBox = document.getElementById("box");
+  var timer = null;
+  oBox.onclick = function() {
+    var i = 0;
+    clearInterval(timer);
+    timer = setInterval(function(){
+      oBox.style.display = i++ % 2 ? "none" : "block";
+      i > 6 && (clearInterval(timer))
+    },80)
+  }
+}
