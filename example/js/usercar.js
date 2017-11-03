a4d970b7036a1e43388c4b1191d791782a6b97d7
liuyanly <liuyan@tongfangcloud.com>
2017-11-03 17:46:52 +0800

    plus minus

--- example/js/usercar.js
+++ example/js/usercar.js
@@ -3,10 +3,10 @@ function Carselect(){
   this.highLightClass = "active";
   this.tempAttrKey = "_key";
   this.initialize();
-  this.num = 1;
 }
 Carselect.prototype.initialize = function(){
   var This = this;
+  var numCar = 1;
   $('div.multi-left ul:first .css_check').each(function(i){
     $(this).click(function(){
       if($(this).parent().hasClass(This.highLightClass)){
@@ -27,11 +27,22 @@ Carselect.prototype.initialize = function(){
     })
     $(this).parent().remove();
   })
-  $('div.multi-left li .icon_minus').click(function(){
-    This.minus();
+  $('div.multi-left li').each(function(){
+    $(this).find('.icon_minus').click(function(){
+      var obj = $(this).parent().find('.num_in');
+      if(numCar <= 1) {
+       obj.text(numCar = 1);
+      }else{
+        obj.text( --numCar);
+      }
+    })
   })
-  $('div.multi-left li .icon_plus').click(function(){
-    This.plus();
+  $('div.multi-left li').each(function(){
+    $(this).find('.icon_plus').click(function(){
+      var obj = $(this).parent().find('.num_in');
+      obj.text(++numCar)
+      console.log(numCar);
+    })
   })
 }
 Carselect.prototype.addChild = function(obj,index){
@@ -42,18 +53,6 @@ Carselect.prototype.addChild = function(obj,index){
   var target = $("<li><span>"+obj.find('.fl').text()+"</span><span>×</span><span>"+obj.find('.num_in').text()+"</span></li>").append(iconClose);
   target.attr(this.tempAttrKey,index);
   this.container.append(target);
-}
-Carselect.prototype.minus = function(){
-  var num = $('div.multi-left li .num_in');
-  if(this.num <= 1) {
-    num.text(this.num = 1);
-  }else{
-    num.text(--this.num);
-  }
-}
-Carselect.prototype.plus = function(){
-  var num = $('div.multi-left li .num_in');
-  num.text(++this.num);
 }
 $(function(){
   var carselect = new Carselect();
